a37be2931d45a996a289b91bc6e65c200baf6290
abhinav warney <abhiwarney@gmail.com>
2024-10-07 18:03:15 +0530

    a

---
+++ 10_Todo_app_local_storage/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-white bg-gray-900 outline-dashed text-center text-3xl p-4 m-4'>TODO APP USING REACT</h1>
+    </>
+  )
+}
+
+export default App
